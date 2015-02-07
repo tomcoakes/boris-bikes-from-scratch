378f7c568a532146d0839f17e304ba23c59b98ed
Tom Coakes <tomcoakes@gmail.com>
2015-02-07 23:28:57 +0000

    Capacity of vans is always 10 and is there from initialization.

--- lib/station.rb
+++ lib/station.rb
@@ -1,10 +1,7 @@
 require_relative 'bike_container'
 
 class Station
-
   include BikeContainer
-
-  DEFAULT_CAPACITY = 10
 
   def initialize(options = {})
     @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
--- lib/van.rb
+++ lib/van.rb
@@ -3,6 +3,10 @@ require_relative 'bike_container'
 class Van
   include BikeContainer
 
+  def initialize
+    @capacity = 10
+  end
+
   def location
     @location ||= "garage"
   end
