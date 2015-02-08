a697f5821e9e5d861b6babb61a6bf9f7b798d768
Tom Coakes <tomcoakes@gmail.com>
2015-02-08 18:39:39 +0000

    Added some comments about my confusion over different ways for classes to interact

--- lib/van.rb
+++ lib/van.rb
@@ -8,10 +8,20 @@ class Van
     @bike_rack = []
   end
 
+
+# THIS METHOD GETS THE STATION TO DO THE RELEASING ON ITS OWN END
   def load_broken_bikes(station)
     station.release_broken_bikes.each do |bike|
       @bike_rack << bike
     end
   end
+
+# AN ALTERNATIVE WAY TO DO THIS WOULD BE AS FOLLOWS, BUT IT CALLS ANOTHER
+# CLASS'S METHOD WITHIN ITS OWN METHOD, WHICH I THOUGHT WAS BAD
+  # def load_broken_bikes(station)
+  #   station.broken_bikes.each do |bike|
+  #     self.dock(station.release(bike))
+  #   end
+  # end
 
 end
